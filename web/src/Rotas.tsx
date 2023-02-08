6df2e40dbc1cbf521ecc8e5fadb634252f8ad0e4
Pedro <phdss04@gmail.com>
2023-02-08 20:27:05 -0300

    ops: adding vercel file

--- web/src/Rotas.tsx
+++ web/src/Rotas.tsx
@@ -7,7 +7,7 @@ import Login from './pages/Login';
 const Rotas = () => {
   return (
     <Routes>
-      <Route path="/login" element={<Login />} />
+      <Route path="/login" index element={<Login />} />
       <Route path="/register" element={<Register />} />
 
       <Route element={<ProtectedRoute route="/login" />}>

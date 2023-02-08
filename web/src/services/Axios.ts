05dcf53dfb42d1fcf82c0547e9f52604d610ac05
Pedro <phdss04@gmail.com>
2023-02-08 20:10:10 -0300

    ops: changing the deploy related urls

--- web/src/services/Axios.ts
+++ web/src/services/Axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// var url = `https://todo-ts-be.up.railway.app/`;
-var url = `http://localhost:3002/`;
+var url = `https://todo-ts-be.up.railway.app/`;
+// var url = `http://localhost:3002/`;
 export const requestAPI = axios.create({
   baseURL: url,
 });
